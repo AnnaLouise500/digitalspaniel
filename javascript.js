a3cd7901886113cec7f97caf3e937149bb9552c8
AnnaLouise500 <annamorrison500@hotmail.com>
2020-02-19 21:41:03 +0000

    portfolio piece almost complete with css.
    page positioning needs work. need to tidy up class names and put gradient and text over image on tablink

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function openProject(evt, projectName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("project");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-bottombar", "");
+  }
+  document.getElementById(projectName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-bottombar";
+}
